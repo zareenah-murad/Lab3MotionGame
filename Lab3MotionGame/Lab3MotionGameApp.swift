593f16b4bfcac3e6b3d794d300487babb84708bc
zareenah <zmurad@smu.edu>
2024-10-17 22:43:14 -0500

    Initial Commit

---
+++ Lab3MotionGame/Lab3MotionGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab3MotionGameApp.swift
+//  Lab3MotionGame
+//
+//  Created by Zareenah Murad on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab3MotionGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
